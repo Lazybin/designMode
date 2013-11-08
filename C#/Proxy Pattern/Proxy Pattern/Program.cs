02927f6977fb938dcf1a7ccc3b2c5f54daf9a577
ab <396887725@qq.com>
2013-11-08 15:19:41 +0800

    Proxy Pattern

---
+++ C#/Proxy Pattern/Proxy Pattern/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolGirl baby = new SchoolGirl();
+            baby.Name = "baby";
+
+            Proxy daili = new Proxy(baby);
+            daili.GiveChocolate();
+            daili.GiveDolls();
+            daili.GiveFlowers();
+            Console.Read();
+        }
+    }
+    class SchoolGirl
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+    interface GiveGift
+    {
+        void GiveDolls();
+        void GiveFlowers();
+        void GiveChocolate();
+    }
+    class Pursuit : GiveGift
+    {
+        SchoolGirl mm;
+        public Pursuit(SchoolGirl mm)
+        {
+            this.mm = mm;
+        }
+        public void GiveDolls()
+        {
+            Console.WriteLine(mm.Name+"送你洋娃娃");
+        }
+        public void GiveFlowers()
+        {
+            Console.WriteLine(mm.Name + "送你鲜花");
+        }
+        public void GiveChocolate()
+        {
+            Console.WriteLine(mm.Name + "送你巧克力");
+        }
+
+    }
+    class Proxy : GiveGift
+    {
+        Pursuit gg;
+        public Proxy(SchoolGirl mm)
+        {
+            gg = new Pursuit(mm);
+        }
+        public void GiveDolls()
+        {
+            gg.GiveDolls();
+        }
+        public void GiveFlowers()
+        {
+            gg.GiveFlowers();
+        }
+        public void GiveChocolate()
+        {
+            gg.GiveChocolate();
+        }
+    }
+}
